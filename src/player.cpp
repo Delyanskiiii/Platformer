45160814b19c55c4aeac8deb68c8608b08f853e7
Delyanskiiii <82833956+Delyanskiiii@users.noreply.github.com>
2023-09-16 17:06:49 +0200

    Fixed movement error

--- src/player.cpp
+++ src/player.cpp
@@ -6,7 +6,7 @@ Player::Player()
 {
     size.Set(10, 10);
     position.Set(100, 100);
-    velocity.Set(10, 10);
+    velocity.Set(3, 3);
     input.Set(0, 0);
     // Pair pastVelocity[100];
 }
@@ -16,10 +16,10 @@ void Player::Update()
     position.value1 += velocity.value1;
     position.value2 += velocity.value2;
 
-    if (position.value1 + size.value1 / 2 >= GetScreenWidth() || position.value1 - size.value1 / 2 <= GetScreenWidth())
+    if (position.value1 + size.value1 / 2 >= GetScreenWidth() || position.value1 - size.value1 / 2 <= 0)
         velocity.value1 *= -1;
 
-    if (position.value2 + size.value2 / 2 >= GetScreenHeight() || position.value2 - size.value2 / 2 <= GetScreenHeight())
+    if (position.value2 + size.value2 / 2 >= GetScreenHeight() || position.value2 - size.value2 / 2 <= 0)
         velocity.value2 *= -1;
 }
 
