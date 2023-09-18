dc78f9c2886881d1aa09f0d6331cbb4df4aa5011
Delyanskiiii <82833956+Delyanskiiii@users.noreply.github.com>
2023-09-18 20:27:28 +0200

    input documentation

--- src/input.cpp
+++ src/input.cpp
@@ -5,9 +5,22 @@ Input::Input() {
     prevInput.Set(0, 0);
 }
 
+/**
+ * Given four booleans that represent if the buttons are being
+ * held down, it calculates the direction intended by the player.
+ * 
+ * @param Left Intended key for moving horizontally to the left.
+ * @param Right Intended key for moving horizontally to the right.
+ * @param Up Intended key for moving vertically up.
+ * @param Down Intended key for moving vertically down.
+ * 
+ * @return Pair containing the input as value1 represents horizontal
+ * direction(Left = -1, 0, Right = 1) and value2 represents vertical(Down = -1, 0, Up = 1).
+ */
 Pair Input::GetInput(bool Left, bool Right, bool Up, bool Down) {
     if (Left && Right)
     {
+        // Checks for prevInput so it doesn't switch directions every frame when both keys are pressed. That's used because pressing left while right is being held the character will swap directions.
         if (prevInput.value1 == 1)
         {
             if (input.value1 == 1)
