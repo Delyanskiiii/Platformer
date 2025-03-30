fe39df710ed4e40b16533509123e9db5cddf02ec
Delyanskiiii <82833956+Delyanskiiii@users.noreply.github.com>
2025-03-31 00:38:18 +0200

    back up bullshit

--- shaders/shadow.fs
+++ shaders/shadow.fs
@@ -33,22 +33,87 @@ float Shadow(int pixelAlpha, ivec2 lightLocation, ivec2 pixelLocation) {
     float a = float(pixelLocation.y - lightLocation.y) / (pixelLocation.x - lightLocation.x);
     float b = pixelLocation.y - a * pixelLocation.x;
     ivec2 currentPixelLocation = lightLocation;
-    int height;
+    float height = 0;
+    float fight = 0;
+    bool direction = false;
 
     while (currentPixelLocation.x != pixelLocation.x || currentPixelLocation.y != pixelLocation.y) {
 
         if (currentPixelLocation.y == pixelLocation.y || floor((currentPixelLocation.x + variance.x) * a + b + 0.5) == currentPixelLocation.y) {
             currentPixelLocation.x += variance.x;
+            direction = true;
+            // if (height < Alpha(texelFetch(Texture, currentPixelLocation, 0).a) - pixelAlpha) {
+            //     direction = true;
+            // }
         } else {
             currentPixelLocation.y += variance.y;
+            direction = false;
+            // if (height < Alpha(texelFetch(Texture, currentPixelLocation, 0).a) - pixelAlpha) {
+            //     direction = false;
+            // }
         }
 
         height = Alpha(texelFetch(Texture, currentPixelLocation, 0).a) - pixelAlpha;
 
         if (height > 0) {
+            // if (abs(pixelLocation.x - currentPixelLocation.x) <= height && abs(pixelLocation.y - currentPixelLocation.y) <= height) {
+            //     return 10;
+            // }
+
+            //  - (height * Distance(lightLocation, currentPixelLocation) / 21) % 10
+
+            // if (int(floor(Distance(lightLocation, currentPixelLocation) * height / 200 + 0.5)) < height) {
+            //     height = int(floor(Distance(lightLocation, currentPixelLocation) * height / 200 + 0.5));
+            // }
+
+            if (Distance(lightLocation, currentPixelLocation) * height / 200 + 1 < height) {
+                height = Distance(lightLocation, currentPixelLocation) * height / 200 + 1;
+            }
+
+            // if (direction) {
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= fight && abs(pixelLocation.y - currentPixelLocation.y) <= fight) {
+            //         return 10;
+            //     }
+            // } else {
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= fight && abs(pixelLocation.y - currentPixelLocation.y) <= fight) {
+            //         return 10;
+            //     }
+            // }
+
             if (abs(pixelLocation.x - currentPixelLocation.x) <= height && abs(pixelLocation.y - currentPixelLocation.y) <= height) {
                 return 10;
             }
+            
+            // if (height >= Distance(pixelLocation, currentPixelLocation)) {
+            //     return 10;
+            // }
+
+
+            // if (direction) {
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= height - abs(lightLocation.x - currentPixelLocation.x) / 3 && abs(pixelLocation.y - currentPixelLocation.y) <= height) {
+            //         return 10;
+            //     }
+            // } else {
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= height && abs(pixelLocation.y - currentPixelLocation.y) <= height - abs(lightLocation.y - currentPixelLocation.y) / 3) {
+            //         return 10;
+            //     }
+            // }
+
+            // if (direction) {
+            //     if (abs(lightLocation.x - currentPixelLocation.x) - height < height) {
+            //         fight = abs(lightLocation.x - currentPixelLocation.x) - height;
+            //     }
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= fight && abs(pixelLocation.y - currentPixelLocation.y) <= height) {
+            //         return 10;
+            //     }
+            // } else {
+            //     if (abs(lightLocation.y - currentPixelLocation.y) - height < height) {
+            //         fight = abs(lightLocation.y - currentPixelLocation.y) - height;
+            //     }
+            //     if (abs(pixelLocation.x - currentPixelLocation.x) <= height && abs(pixelLocation.y - currentPixelLocation.y) <= fight) {
+            //         return 10;
+            //     }
+            // }
         }
     }
     return 0;
